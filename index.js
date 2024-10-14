37842de5e8707943cb732b0cedfb33fb0c936bf6
zdunas82 <zdoonas82@gmail.com>
2024-10-14 23:19:41 +0100

    update

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ import cowsay from "cowsay"
 console.log(chalk.blue("hello world!"));
 console.log(chalk.green("Green Hello World!"));
 
+
 console.log(cowsay.say({
 text: "I'm a mooodule",
 e : "o0",
 T : "U ",
 }));
+
+
+const error = chalk.bold.red;
+const warning = chalk.hex('#FFA500') //orange colour
+
+console.log(error("Error!"));
+console.log(warning("Warning!"));
+
+const cowMassage = cowsay.say({
+  text: "I'm mooodule!",
+  e: "o0",
+  T: "U "
+});
+
+console.log(chalk.green(cowMassage));
+
+
