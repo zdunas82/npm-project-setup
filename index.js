1b0990a9c2d9601aafecbcc9b7bbd803dddc99dd
zdunas82 <zdoonas82@gmail.com>
2024-10-14 15:59:09 +0100

    added cowsay module

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import chalk from "chalk";
+import cowsay from "cowsay"
 
 console.log(chalk.blue("hello world!"));
 console.log(chalk.green("Green Hello World!"));
 
+console.log(cowsay.say({
+text: "I'm a mooodule",
+e : "o0",
+T : "U ",
+}));
